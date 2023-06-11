ec0ae2148d933cc5a3e1ecf68e6b7246856f6118
yamato0811 <yama8to11@gmail.com>
2023-06-11 11:08:56 +0900

    first commit

---
+++ app.go
@@ -0,0 +1,10 @@
+package main
+import ( 
+    "net/http"
+    "github.com/gin-gonic/gin"
+)
+func main() { 
+    r := gin.Default() 
+    r.GET("/", func(c *gin.Context) { c.String(http.StatusOK, "Hello World!!") })
+    r.Run() // gin run app.go
+}
