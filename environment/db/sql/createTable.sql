28fb3195d6c1442b75eaf405ab672bf4a5b6ca07
yamato0811 <yama8to11@gmail.com>
2023-06-11 23:01:06 +0900

    database修正

--- environment/db/sql/createTable.sql
+++ environment/db/sql/createTable.sql
@@ -1,8 +1,9 @@
 CREATE DATABASE IF NOT EXISTS gdb;
 USE gdb;
 
-CREATE TABLE IF NOT EXISTS groups (
+CREATE TABLE IF NOT EXISTS todos (
     `id` int(11) NOT NULL AUTO_INCREMENT,
-    `name` varchar(255) NOT NULL,
+    `title` varchar(255) NOT NULL,
+    `completed` tinyint(1) NOT NULL DEFAULT '0',
     PRIMARY KEY (`id`)
 );
